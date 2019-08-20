906e10c4c5d6efc11787163ffdf7823c3839da63
hando <enthusapp@gmail.com>
2019-08-20 10:19:48 +0900

    plan control test sucess

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import SerialPort from 'serialport';
 
-const port = new SerialPort('COM23', { baudRate: 9600 });
+const portName = 'COM23';
+
+const port = new SerialPort(portName, { baudRate: 9600 });
+
+const planNumber = 2;
+
+const command = [
+  0x02,
+  0xFA,
+  0xFF,
+  0xFF,
+  0xF9,
+  planNumber,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+];
+
+const msg = new Uint8Array(
+  command.concat([command.reduce((a, c) => a + c, 0), 0xF7, 0x03]),
+);
 
 port.once('open', () => {
-  console.log('opened');
+  console.log(`${portName} opened`);
+  console.log(`Send ${msg}`);
+  port.write(msg, (err) => {
+    if (err) console.log(err);
+  });
 });
-
-function MFAlarmStart() {
-
-}
-
-MFAlarmStart();
